94d172f3fcaf196f587edbf1c2c7dff621ab5e97
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-04-28 16:52:57 -0700

    new changes on 428

--- application/views/news/success.php
+++ application/views/news/success.php
@@ -1,2 +1,10 @@
-<h1>Success!</h1>
-<p>You have successfully entered your news item.</p>
+<?php
+//$this->load->view('themes/bootswatch/header');
+$this->load->view($this->config->item('theme') . 'header');
+?>
+<?php
+echo '<h2>Success!</h2>';
+
+//$this->load->view('themes/bootswatch/header');
+$this->load->view($this->config->item('theme') . 'footer');
+?>
