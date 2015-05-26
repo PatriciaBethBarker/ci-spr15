303860676ccd5c5704f7b5266e012cc40a7c46af
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-05-26 15:18:40 -0700

    add changes

--- application/config/custom_config.php
+++ application/config/custom_config.php
@@ -1,9 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-//custom_config.php - a place to save global settings
+//custom_config.php - save global settings here
+/**
+ * Custom_config.php
+ * Config file for hooking up theme
+ */
 
-$config['style'] = 'flatly.css';
-$config['banner'] = 'Default Banner';
-$config['title'] = 'Default Title';
-$config['copyright'] = 'Default Copyright';
-$config['masthead'] = 'Default MastHead';
-$config['theme'] = 'themes/bootswatch/';
+ $config['style'] = 'flatly.css';
+ $config['banner'] = 'Default Banner';
+ $config['title'] = 'Default Title';
+ $config['copyright'] = 'Default Copyright';
+ $config['masthead'] = 'Default MastHead'; 
+ $config['theme'] = 'themes/bootswatch/';
+ $config['banner-href'] = './';
